6fa7221deedd0696a507b80aad50a9cfd79b118a
naveennathani1 <naveennathani@sosonweb.com>
2024-04-22 11:17:42 -0500

    change VPC name

--- main.tf
+++ main.tf
@@ -61,8 +61,8 @@ resource "aws_route_table" "private_route_table" {
 }
 
 resource "aws_route_table_association" "private_subnet_association" {
-  subnet_id      = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_1.id]
-  route_table_id = aws_route_table.private_route_table_1.id
+  subnet_id      = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_eip" "nat_eip" {
